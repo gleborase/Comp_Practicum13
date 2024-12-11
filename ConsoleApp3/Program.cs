d46a82e84e8067582432cf3b2f27735f5c290d55
gleborase <krasnopolsci@gmail.com>
2024-12-11 10:21:22 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// Задача 3. По номеру месяца вывести название времени года.
+    /// </summary>
+    internal class Program
+    {
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int s = Input("month=");
+            Console.WriteLine($"Время года: {GetMonth(s)}");
+            Console.Read();
+
+        }
+        static string GetMonth(int month)
+        {
+            switch (month)
+            {
+                case 1: return "Зима";
+                case 2: return "Зима";
+                case 3: return "Весна";
+                case 4: return "Весна";
+                case 5: return "Весна";
+                case 6: return "Лето";
+                case 7: return "Лето";
+                case 8: return "Лето";
+                case 9: return "Осень";
+                case 10: return "Осень";
+                case 11: return "Осень";
+                case 12: return "Зима";
+                default: return "Ошибка";
+            }
+        }
+    }
+}
