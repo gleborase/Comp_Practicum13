8dd1cfcfd3163fe257efdfe7e14e8cd0cc760297
gleborase <krasnopolsci@gmail.com>
2024-12-11 10:01:56 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// Задача 2. По номеру месяца вывести его название.
+    /// </summary>
+    internal class Program
+    {
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int month = Input("month=");
+            string m = GetMonth(month);
+            Console.WriteLine($"Месяц: {m}");
+            Console.Read();
+
+        }
+        static string GetMonth(int month)
+        {
+            switch (month)
+            {
+                case 1: return "Январь";
+                case 2: return "Февраль";
+                case 3: return "Март";
+                case 4: return "Апрель";
+                case 5: return "Май";
+                case 6: return "Июнь";
+                case 7: return "Июль";
+                case 8: return "Август";
+                case 9: return "Сентябрь";
+                case 10: return "Октябрь";
+                case 11: return "Ноябрь";
+                case 12: return "Декабрь";
+                default: return "Ошибка";
+
+
+            }
+        }
+    }
+}
+
+    
+
